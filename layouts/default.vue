ce79523b64941c40dd317b3dd7f5af169079066a
detextre4 <detextre4@gmail.com>
2022-11-05 19:34:11 -0400

    floating swap button

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,10 @@
     <Alerts ref="alerts"></Alerts>
     <Navbar ref="navbar" />
     <v-main :class="wrapperSpace?'with':'without'" class="parent">
+      <v-btn v-show="$route.path !== '/swap'" id="swap-floating-button" class="btn">
+        <v-icon>mdi-chevron-up</v-icon>
+        <span>Swap</span>
+      </v-btn>
       <nuxt-child />
     </v-main>
     <!-- <Footer ref="footer"></Footer> -->
@@ -18,11 +22,17 @@ export default {
       wrapperSpace: false,
     }
   },
+  watch: {
+    $route() {
+      this.dragFloatingBtn();
+    }
+  },
   created() {
     // get data profile
     this.$store.dispatch("getData");
   },
   mounted() {
+    this.dragFloatingBtn();
     this.scrollX();
     // this.footerHeightListener();
     
@@ -30,7 +40,7 @@ export default {
     // window.addEventListener("resize", this.footerHeightListener);
   },
   beforeDestroy() {
-    window.removeEventListener("resize", this.footerHeightListener);
+    // window.removeEventListener("resize", this.footerHeightListener);
   },
   methods: {
     scrollX() {
@@ -58,6 +68,47 @@ export default {
         );
       }, 400);
     },
+    dragFloatingBtn() {
+      const target = document.getElementById ("swap-floating-button");
+      let offset = [0,0], isDown = false;
+      function typeEvent(event) {
+        if (event.type.includes('mouse')) {
+          return event
+        } else if (event.type.includes('touch')) {
+          return event.touches[0]
+        }
+      }
+
+      const mousedown = (e) => {
+        if (this.$route.path !== '/swap') {
+          isDown = true;
+          offset = [
+            target.offsetTop - typeEvent(e).clientY
+          ];
+        }
+      }
+      const mousemove = (e) => {
+        if (this.$route.path !== '/swap') {
+          const
+            position = typeEvent(e).clientY + offset[0],
+            range = 100;
+          if (e.type.includes('mouse')) e.preventDefault();
+          if (isDown && position > range && position < window.innerHeight - (range + 80)) target.style.top  = `${position}px`;
+        }
+      }
+      const mouseup = () => { 
+        if (this.$route.path !== '/swap') { isDown = false }
+      }
+
+      // desktop
+      target.addEventListener('mousedown', function(e) { mousedown(e) }, true);
+      document.addEventListener('mouseup', function() { mouseup() }, true);
+      document.addEventListener('mousemove', function(e) { mousemove(e) }, true);
+      // mobile
+      target.addEventListener('touchstart', function(e) { mousedown(e) }, true);
+      document.addEventListener('touchend', function() { mouseup() }, true);
+      document.addEventListener('touchmove', function(e) { mousemove(e) }, true);
+    }
   },
 }
 </script>
