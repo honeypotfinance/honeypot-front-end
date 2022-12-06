4163ee464c4d38088974a49021e4d98560f44410
detextre4 <detextre4@gmail.com>
2022-12-06 16:25:11 -0400

    fixes

--- components/menu-navbar.vue
+++ components/menu-navbar.vue
@@ -1,96 +1,75 @@
 <template>
-  <div id="menuNeader">
+  <div id="menuNavbar">
     <!--////////////// drawers //////////////-->
-    <!-- toggle drawer -->
-    <v-navigation-drawer
-      v-model="drawer"
-      height="100%"
-      fixed right
-      temporary
-      :overlay-opacity="$store.state.overlay.opacity"
-      :overlay-color="$store.state.overlay.color"
-      class="font2"
-    >
-      <section class="v-navigation-drawer__content--navbar divcol center gap2">
-        <v-btn icon class="close" style="--t: 8px; --r: 10px" @click="drawer=false">
-          <img src="~/assets/sources/icons/close.svg" alt="close" style="--w:2.375em">
-        </v-btn>
-        
+    <CustomeDrawer :model="drawer" @update="drawer = $event">
+      <template #header>
         <a class="center" @click="$router.push(localePath('/')); $scrollTo('home')">
-          <img src="~/assets/sources/icons/records.svg" alt="logo" style="--w: 6em">
+          <img src="~/assets/sources/logos/logo.svg" alt="logo" style="--w: 100%">
         </a>
-        <v-btn class="btn" style="--w:75%; --min-h: 30px; --p: .5em 2em" :ripple="false">Connect</v-btn>
-      </section>
-
-      <section class="v-navigation-drawer__content--content divcol jspace gap2">
-        <v-expansion-panels focusable accordion class="anim_moveleft">
-          <template v-if="dataDrawer.expansion">
-            <v-expansion-panel v-for="(item, index) in dataDrawer.expansion" :key="index">
-              <!-- title -->
-              <v-expansion-panel-header class="conttitle acenter gap1 h10_em" hide-actions @click="ActiveClass('expansion', item)">
-                  <img :src="require(`~/assets/sources/icons/${item.key}${item.active?'-active':''}.svg`)" :alt="`${item.key} icon`" class="icon" :class="{active: item.active}">
-                  <span class="normal" style="max-width: max-content">{{ item.name }}</span>
-                  <v-icon small color="#ffffff" :class="{reverse: item.active}">mdi-menu-down</v-icon>
-              </v-expansion-panel-header>
-
-              <v-expansion-panel-content>
-                <v-list>
-                  <!-- ciclo for items -->
-                  <v-list-item-group active-class="activeText">
-                    <v-list-item v-for="(item2,i) in item.selection" :key="i" :ripple="false" :to="localePath(item2.to)" @click="drawer = false">
-                      <v-list-item-title class="center h10_em">
-                        <span class="normal">{{ item2.name}}</span>
-                      </v-list-item-title>
-                    </v-list-item>
-                  </v-list-item-group>
-                </v-list>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </template>
-
-
-          <template v-if="dataDrawer.list">
-            <v-list class="fill_w">
-              <!-- ciclo for items -->
-              <v-list-item v-for="(item,i) in dataDrawer.list" :key="i" link :to="localePath(item.to)" :ripple="false">
-                <v-list-item-title class="conttitle acenter gap1 h10_em" @click="ActiveClass('list', item); drawer = false">
-                    <img :src="require(`~/assets/sources/icons/${item.key}${item.active?'-active':''}.svg`)" class="icon" :alt="`${item.key} icon`" :class="{active: item.active}">
-                    <span style="max-width: max-content">
-                      {{ item.name }}
-                    </span>
-                </v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </template>
-        </v-expansion-panels>
-
-        <section class="v-navigation-drawer__content--footer divcol center">
-          <span class="h10_em clr_inv">Join us on:</span>
-
-          <div class="center">
-            <v-btn v-for="(item,i) in dataSocial" :key="i" icon :href="item.url" target="_blank">
-              <img :src="require(`~/assets/sources/icons/${item.icon}.svg`)" alt="social red">
+        
+        <v-btn
+          v-if="!$parent.isLogged" :ripple="false"
+          class="btn" style="--w: 75%; --min-h: 30px; --p: .5em 2em"
+          @click="$store.dispatch('modalConnect')"
+        >Connect</v-btn>
+        
+        <!-- menu login -->
+        <v-menu v-else bottom offset-y nudge-bottom="10px">
+          <template #activator="{ on, attrs }">
+            <v-btn class="btn2" style="--bg: var(--accent)" v-bind="attrs" v-on="on">
+              <span>{{$parent.user.accountId}}</span>
+              <v-icon>mdi-chevron-down</v-icon>
             </v-btn>
-          </div>
-        </section>
-      </section>
-    </v-navigation-drawer>
+          </template>
+
+          <v-list color="var(--accent)" style="--c:#fff">
+            <v-list-item
+              v-for="(item,i) in $parent.dataMenuLogin" :key="i"
+              @click="item.key==='logout' ? $store.commit('signOut') : $router.push(localePath(key))">
+              <v-list-item-title>{{item.name}}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </template>
 
 
+      <template #content>
+        <v-expansion-panels focusable accordion class="anim_moveleft">
+          <v-expansion-panel
+            v-for="(item, i) in $parent.dataNavbar" :key="i"
+            @click="item.name !== 'portfolio' ? $router.push(localePath(item.to)) : ''"
+          >
+            <v-expansion-panel-header class="h10_em" expand-icon="mdi-menu-down" :hide-actions="item.to ? true : false">
+              {{ item.name }}
+            </v-expansion-panel-header>
 
-    <!--////////////// menues //////////////-->
-    <!-- menu login -->
-    <v-menu activator=".openMenuLogin" bottom offset-y>
-      <v-list class="font2" color="var(--secondary)" style="--c:#fff">
-        <v-list-item-group active-class="activeClass">
-          <v-list-item
-            v-for="(item,i) in dataMenuLogin" :key="i"
-            @click="item.key==='logout' ? $store.commit('signOut') : $router.push(localePath(key))">
-            <v-list-item-title>{{item.name}}</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-menu>
+            <v-expansion-panel-content v-if="!item.to">
+              <v-list>
+                <v-list-item
+                  v-for="(item2,i2) in item.selection" :key="i2" :ripple="false"
+                  :to="localePath(item2.to)" @click="drawer = false"
+                >
+                  <v-list-item-title class="center h10_em">
+                    <span>{{ item2.name}}</span>
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </template>
+
+
+      <template #footer>
+        <span class="h10_em clr_inv">Join us on:</span>
+
+        <div class="center">
+          <v-btn v-for="(item,i) in dataSocial" :key="i" icon :href="item.url" target="_blank">
+            <img :src="require(`~/assets/sources/icons/${item.icon}.svg`)" alt="social red">
+          </v-btn>
+        </div>
+      </template>
+    </CustomeDrawer>
   </div>
 </template>
 
@@ -99,74 +78,15 @@ export default {
   name: "NavbarMenuComponent",
   data() {
     return {
-      messages: 1,
       drawer: false,
-      dataDrawer: {
-        list: [
-          {
-            key: "stats",
-            name: "STATS",
-            to: "/stats",
-            active: false,
-          },
-          {
-            key: "chats",
-            name: "CHATS",
-            to: "/chats",
-            active: false,
-          },
-          {
-            key: "settings",
-            name: "SETTINGS",
-            to: "/settings",
-            active: false,
-          },
-          {
-            key: "faq",
-            name: "FAQ",
-            to: "/faq",
-            active: false,
-          },
-        ],
-        expansion: [
-          {
-            key: "market",
-            name: "MARKETPLACE",
-            active: false,
-            selection: [
-              {key: "buy", name: "Buy", to: "/buy"},
-              {key: "sell", name: "Sell", to: "/sell"},
-            ],
-          },
-        ],
-      },
       dataSocial: [
         { icon:"twitter", url:"#" },
         { icon:"instagram", url:"#" },
         { icon:"twitch", url:"#" }
       ],
-      dataMenuLogin: [
-        { key:"logout", name:"Log out" },
-      ],
     };
   },
   methods: {
-    ActiveClass(key, item) {
-      // mejorar hace falta
-      if (key === 'expansion') {
-        if (item.active) {
-          item.active=!item.active
-        } else {
-          this.dataDrawer.list.forEach(e=>{e.active=false})
-          this.dataDrawer.expansion.forEach(e=>{e.active=false})
-          item.active=true
-        }
-      } else if (key === 'list') {
-        this.dataDrawer.expansion.forEach(e=>{e.active=false})
-        this.dataDrawer.list.forEach(e=>{e.active=false})
-        item.active=true
-      }
-    },
   },
 };
 </script>
--- components/navbar.vue
+++ components/navbar.vue
@@ -3,14 +3,16 @@
     <MenuNavbar ref="menu"></MenuNavbar>
     
     <v-app-bar id="navbar" color="transparent" absolute class="isolate">
-      <nuxt-link to="/swap">
+      <!-- desktop -->
+      <nuxt-link class="deletemobile" :to="localePath('/swap')">
         <img src="~/assets/sources/logos/logo.svg" alt="logo" style="--w: clamp(10em, 13vw, 13.414375em)">
       </nuxt-link>
+      <!-- mobile -->
+      <nuxt-link class="showmobile" :to="localePath('/swap')">
+        <img src="~/assets/sources/logos/logo-mobile.svg" alt="logo" style="--w: 40px">
+      </nuxt-link>
 
-      <!-- <v-btn class="btn showmobile" @click="$refs.menu.drawer = true">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn> -->
-
+      <!-- desktop -->
       <aside class="middle tcap deletemobile">
         <a
           v-for="(item, i) in dataNavbar" :key="i"
@@ -20,18 +22,40 @@
         </a>
       </aside>
 
-      <aside class="right" :class="isLogged ? 'font2' : 'font1'">
+      <!-- desktop -->
+      <aside class="right deletemobile" :class="isLogged ? 'font2' : 'font1'">
         <v-btn class="btn2">
           <img src="~/assets/sources/logos/honeypot.svg" alt="token" class="aspect" style="--w: 1.533125em">
           <span>${{user.balance}}</span>
         </v-btn>
+
+        <v-btn v-if="!isLogged" class="btn2" @click="$store.dispatch('modalConnect')">Connect wallet</v-btn>
         
-        <v-btn v-show="!isLogged" class="btn2" @click="$store.dispatch('modalConnect')">Connect wallet</v-btn>
-        <v-btn v-show="isLogged" class="btn2 openMenuLogin">
-          <span>{{user.accountId}}</span>
-          <v-icon>mdi-chevron-down</v-icon>
-        </v-btn>
+        <!-- menu login -->
+        <v-menu v-else bottom offset-y nudge-bottom="10px">
+          <template #activator="{ on, attrs }">
+            <v-btn class="btn2" v-bind="attrs" v-on="on">
+              <span>{{user.accountId}}</span>
+              <v-icon>mdi-chevron-down</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list class="font2" color="var(--secondary)" style="--c:#fff">
+            <v-list-item-group active-class="activeClass">
+              <v-list-item
+                v-for="(item,i) in dataMenuLogin" :key="i"
+                @click="item.key==='logout' ? $store.commit('signOut') : $router.push(localePath(key))">
+                <v-list-item-title>{{item.name}}</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-menu>
       </aside>
+
+      <!-- mobile -->
+      <v-btn icon class="showmobile" @click="$refs.menu.drawer = true">
+        <v-icon large>mdi-menu</v-icon>
+      </v-btn>
     </v-app-bar>
   </div>
 </template>
@@ -65,6 +89,9 @@ export default {
           name: "xhpot",
           to: "/staking"
         },
+      ],
+      dataMenuLogin: [
+        { key:"logout", name:"Log out" },
       ],
     };
   },
